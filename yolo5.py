af1dd5ee3cfb0682fc66f5d90d32023646d3a4a0
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2023-02-11 21:25:28 +0500

    Add files via upload

---
+++ yolo5.py
@@ -0,0 +1,54 @@
+import torch
+import cv2
+import numpy as np
+from tracker import *
+
+
+
+def POINTS(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('ROI')
+cv2.setMouseCallback('ROI', POINTS)
+
+
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+count=0
+cap=cv2.VideoCapture('tvid.mp4')
+
+tracker=Tracker()
+
+while True:
+    ret,frame=cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    
+    frame=cv2.resize(frame,(1020,600))
+    results = model(frame)
+    list=[]
+    for index, row in results.pandas().xyxy[0].iterrows():
+        x1 = int(row['xmin'])
+        y1 = int(row['ymin'])
+        x2 = int(row['xmax'])
+        y2 = int(row['ymax'])
+        d=(row['name'])
+#        print(d)
+     
+        if 'motorcycle' in d:
+            cv2.rectangle(frame,(x1,y1),(x2,y2),(255,0,0),2)
+            cv2.putText(frame,str(d),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,255),1)
+        
+          
+        
+    cv2.imshow("ROI",frame)
+    if cv2.waitKey(1)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
